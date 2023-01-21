859a1cfc347cfa4a734bd312f815ba0d2aa44b0a
Felipe Silva Rossi <32377360+FelipeSRossi@users.noreply.github.com>
2023-01-21 16:17:21 -0300

    First Commit
    First Commit

---
+++ Source/NeonFable/BaseEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "BaseEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEONFABLE_API ABaseEnemy : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
